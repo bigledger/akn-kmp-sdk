9767125b5a8deef5cfca6094f5d23ba0aa177994
Shyam Borkar <shyam.borkar108@gmail.com>
2025-11-05 10:42:36 +0800

    Initial kmp library files

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "akn-kmp-client-sdk"
+include(":akn-kmp-client-sdk")
